4b4326dbd941dd4225f85912c2b9512d65041939
github_Jbhupp <jenni.hupp@bryanuniversity.edu>
2023-10-01 17:20:54 -0400

    commit

--- Capstone/Django/business/views.py
+++ Capstone/Django/business/views.py
@@ -1,7 +1,18 @@
-from django.http import HttpResponse
-from django.template import loader
 
-def business(request):
-    template = loader.get_template('home.html')
-    return HttpResponse(template.render())
+from django.shortcuts import render
+from business import *
 
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def random(request):
+    return render(request, 'random.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def admin(request):
+    return render(request, 'admin.html')
