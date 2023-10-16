8523a88842d12a9302078812ec58a83a0a1ae976
github_Jbhupp <jenni.hupp@bryanuniversity.edu>
2023-10-16 19:01:36 -0400

    commit

--- soc/shirtzone/business/project/tests.py
+++ soc/shirtzone/business/project/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import Client
+
+
+class PageViewTests(TestCase):
+
+    c = Client()
+
+    def test_page_status_code(self):
+        response = self.c.get('/about', follow= True)
+        self.assertEquals(response.status_code, 200)
